c935847bededb38ab358d80ea94205469d1ca406
TonyPFrancis <tony.612.4u@gmail.com>
2022-09-30 15:26:42 +0530

    adding version

--- setup.py
+++ setup.py
@@ -1,16 +1,27 @@
+import os
 import pathlib
+
 from setuptools import setup
 
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
-
+NAME = "article_api"
 # The text of the README file
 README = (HERE / "README.md").read_text()
+
+
+def get_version():
+    about = {}
+    here = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(here, NAME.replace("-", "_"), "__version__.py")) as f:
+        exec(f.read(), about)
+    return about["__version__"]
+
 
 # This call to setup() does all the work
 setup(
     name="article-api-test",
-    version="1.0.0",
+    version=get_version(),
     description="Get basic details from Article URL",
     long_description=README,
     long_description_content_type="text/markdown",
