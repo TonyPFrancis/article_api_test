7423dee2f1b1ea4bd0e9708c898a3013b9d62616
TonyPFrancis <tony.612.4u@gmail.com>
2022-09-30 15:38:50 +0530

    updating test

--- article_api/__init__.py
+++
@@ -1,2 +0,0 @@
-# Version of the article-api package
-__version__ = "1.0.0"
--- tests/test_url.py
+++ tests/test_url.py
@@ -1,4 +1,5 @@
 import os
+
 print(os.getcwd())
 
 from ..article_api.__main__ import main as main_url
@@ -7,7 +8,7 @@ from ..article_api.__main__ import main as main_url
 def test_url():
     url = "https://www.bbc.com/news/science-environment-61383391"
     crawled_item = {'url': 'https://www.bbc.com/news/science-environment-61383391', 'source_name': 'BBC News',
-                    'source_logo': 'https://www.bbc.co.uk/news/special/2015/newsspec_10857/bbc_news_logo.png?cb=1',
+                    'source_logo': 'https://static.files.bbci.co.uk/ws/simorgh-assets/public/news/images/metadata/poster-1024x576.png',
                     'date': '2022-05-09T23:38:30.000Z',
                     'title': "Climate change: 'Fifty-fifty chance' of breaching 1.5C warming limit",
                     'thumbnail': 'https://ichef.bbci.co.uk/news/1024/branded_news/E4D9/production/_124658585_gettyimages-1240334571.jpg',
